4ac13ba253b6a3486f03305f195c1a4792a6ac1c
Jay Roberts <jay@gloryfish.org>
2010-09-29 17:36:33 -0400

    Challenge 4. Like a BOSS.

---
+++ 4.py
@@ -0,0 +1,21 @@
+# 
+#  4.py
+#  PythonChallenge
+#  
+#  Created by Jay Roberts on 2010-09-29.
+# 
+
+import urllib
+import re  # We learn from the previous lesson
+
+endpoint = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
+number = '12345' # <-- Start here
+# number = str(92118 / 2)
+
+while True:
+    content = urllib.urlopen(endpoint + number).read()
+    print content
+    number = re.findall('\d+', content)[-1] # Only grab the last matched number, those sneaky...
+    print number
+
+print content
