27151c992fbcd986f388720bd39ccd8f7bdb141d
Jay Roberts <jay@gloryfish.org>
2010-10-07 12:22:58 -0400

    Challenge 11 complete

--- 11.py
+++ 11.py
@@ -10,13 +10,13 @@ import urllib
 from PIL import Image
 import string
 
-webfile = urllib.urlopen('http://www.pythonchallenge.com/pc/return/cave.jpg')
-
-localfile = open('cave.jpg', 'w')
-localfile.write(webfile.read())
-
-webfile.close()
-localfile.close()
+# webfile = urllib.urlopen('http://www.pythonchallenge.com/pc/return/cave.jpg')
+# 
+# localfile = open('cave.jpg', 'w')
+# localfile.write(webfile.read())
+# 
+# webfile.close()
+# localfile.close()
 
 img = Image.open('cave.jpg')
 
@@ -29,4 +29,12 @@ imgB = Image.new('RGBA', img.size)
 
 for x in range(0, img.size[0]):
     for y in range(0, img.size[1]):
-        print x + ' ' + y
+        offset = 0
+        if (y % 2 == 0):
+            offset = 1
+        color = img.getpixel((x, y))
+        imgA.putpixel((x / 2, y / 2 ), color)
+
+imgA.save('imageA.jpg')
+
+# Creeepy!!!
