5382fce671bbf4cd16f1d693b75ebc371a90a7ee
Jay Roberts <jay@gloryfish.org>
2010-11-18 14:31:51 -0500

    13 and 13 down

---
+++ 12.py
@@ -0,0 +1,32 @@
+# 
+#  12.py
+#  PythonChallenge
+#  
+#  Created by Jay Roberts on 2010-11-18.
+# 
+
+import urllib
+
+# webfile = urllib.urlopen('http://www.pythonchallenge.com/pc/return/evil2.gfx')
+# 
+# localfile = open('evil2.gfx', 'w')
+# localfile.write(webfile.read())
+# 
+# webfile.close()
+# localfile.close()
+
+gfx = open('evil2.gfx', 'r').read()
+
+# Lots of mucking around with a hex editor and looking up
+# image file formats and you can make out some jpeg 
+# headers in there.
+
+# Break this file up into 5 pieces, by taking a card (byte) 
+# from the top and putting it into a pile (file), then the next
+# card goes to the next pile and so on...
+
+# I originally had this written out the long way but when I was 
+# looking up sequence slicing I learned how to slice stepwise (i::5)
+# which is so cool.
+
+[open('image_%d' % i, 'w').write(gfx[i::5]) for i in range(5)]
---
+++ 13.py
@@ -0,0 +1,22 @@
+# 
+#  13.py
+#  PythonChallenge
+#  
+#  Created by Jay Roberts on 2010-11-18.
+# 
+
+import xmlrpclib
+
+proxy = xmlrpclib.ServerProxy('http://www.pythonchallenge.com/pc/phonebook.php')
+
+# Poked around the interface using the REPL at this point.
+# Got to listMethods(), phone('5'), "He is not the evil"
+
+#  Lots of head banging on desk...
+
+# Looked back through previous "evil" images.
+# evil4.jpg had data but was broken. Hex editor
+# reveals that Bert is evil!
+
+print proxy.phone('Bert')
+
